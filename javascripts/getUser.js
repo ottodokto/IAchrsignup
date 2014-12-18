0176961691a3f775215e06d931d0d456ac01f112
ksjdragon <ksjdragon@gmail.com>
2014-12-18 00:03:47 -0500

    Tried JS, fails.

---
+++ javascripts/getUser.js
@@ -0,0 +1,8 @@
+//to be integrated into complete GUI on login.html
+var form = documentElementByID("login");
+	username = documentElementByID("username");
+login.onsubmit = function() {
+	
+	alert(username);
+}
+//morepractice
